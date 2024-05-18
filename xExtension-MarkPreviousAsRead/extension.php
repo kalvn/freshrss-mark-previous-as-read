b31bcf09ba02b033d2723e54797c25faa4464e19
kalvn <kalvn@pm.me>
2024-05-18 14:31:48 +0200

    fix: button was not showing when feed display isn't icon plus name

--- xExtension-MarkPreviousAsRead/extension.php
+++ xExtension-MarkPreviousAsRead/extension.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 class MarkPreviousAsReadExtension extends Minz_Extension {
 	public function init(): void {
-    Minz_View::appendScript($this->getFileUrl('script.js', 'js'), false, true);
-    Minz_View::appendStyle($this->getFileUrl('style.css', 'css'));
+      Minz_View::appendScript($this->getFileUrl('script.js', 'js'), false, true);
+      Minz_View::appendStyle($this->getFileUrl('style.css', 'css'));
 	}
 }
