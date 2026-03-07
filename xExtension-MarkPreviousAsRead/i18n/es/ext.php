581f0f91c8b91557a7c2ffd7af4f6e4a054798d0
Pablo González Troyano <44492590+gonzaleztroyano@users.noreply.github.com>
2026-03-07 19:09:20 +0100

    Fix newline scaping on i8n files containint "\n"

--- xExtension-MarkPreviousAsRead/i18n/es/ext.php
+++ xExtension-MarkPreviousAsRead/i18n/es/ext.php
@@ -10,8 +10,8 @@ return [
   'js' => [
     'markAllPreviousAsRead' => 'Marcar todas las entradas anteriores como leídas',
     'markedEntriesAsRead' => 'Marcadas {0} entradas como leídas.',
-    'warning' => '¿Seguro que desea marcar como leídas todas las entradas anteriores?\n Esta acción no se puede deshacer.',
-    'warningSameFeed' => '¿Seguro que desea marcar como leídas todas las entradas anteriores de {0}?\nEsta acción no se puede deshacer.',
+    'warning' => "¿Seguro que desea marcar como leídas todas las entradas anteriores?\n Esta acción no se puede deshacer.",
+    'warningSameFeed' => "¿Seguro que desea marcar como leídas todas las entradas anteriores de {0}?\nEsta acción no se puede deshacer.",
     'theSameFeed' => 'la misma fuente RSS'
   ]
 ];
