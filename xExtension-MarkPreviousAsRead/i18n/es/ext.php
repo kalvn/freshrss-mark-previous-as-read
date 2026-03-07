81e369b527576c5737438b27672635b9a685874f
Pablo González Troyano <44492590+gonzaleztroyano@users.noreply.github.com>
2026-03-07 19:03:35 +0100

    Add Spanish (es) i18n

---
+++ xExtension-MarkPreviousAsRead/i18n/es/ext.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+  'config' => [
+    'enableWarningPopup' => 'Mostrar una alerta de popup antes de marcar entradas como leídas.',
+    'applyOnlyToSameFeedEntries' => 'Aplicar solo a entradas que pertenezcan a la misma fuente RSS.',
+    'save' => 'Guardar configuración',
+    'cancel' => 'Cancelar'
+  ],
+  'js' => [
+    'markAllPreviousAsRead' => 'Marcar todas las entradas anteriores como leídas',
+    'markedEntriesAsRead' => 'Marcadas {0} entradas como leídas.',
+    'warning' => '¿Seguro que desea marcar como leídas todas las entradas anteriores?\n Esta acción no se puede deshacer.',
+    'warningSameFeed' => '¿Seguro que desea marcar como leídas todas las entradas anteriores de {0}?\nEsta acción no se puede deshacer.',
+    'theSameFeed' => 'la misma fuente RSS'
+  ]
+];
